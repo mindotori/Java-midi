2778a83b01868710d25c8c9a09f810d9094fc54d
mindotori <148763440+mindotori@users.noreply.github.com>
2024-08-22 21:42:23 +0900

    JAVA: 참조형과 메서드 호출

--- src/ref/Method2.java
+++ src/ref/Method2.java
@@ -1,22 +1,25 @@
 package ref;
 
-public class Method1 {
+public class Method2 {
 
     public static void main(String[] args) {
-        Student student1 = new Student();
-        initStudent(student1, "학생1",15,90);
-
-        Student student2 = new Student();
-        initStudent(student2, "학생2", 16,80);
+        Student student1 = createStudent("학생1",15,90);
+        Student student2 = createStudent("학생2",16,80);
 
         printStudent(student1);
         printStudent(student2);
     }
-    //initStudent: 전달한 학생 객체의 필드에 값을 설정한다.
-    static void initStudent(Student student, String name, int age, int grade){
+
+    static Student createStudent(String name,int age,int grade){
+        Student student = new Student();
         student.name = name;
         student.age = age;
         student.grade = grade;
+        return student;
+    }
+
+    //initStudent: 전달한 학생 객체의 필드에 값을 설정한다.
+    static void initStudent(Student student, String name, int age, int grade){
     }
 
     //printStudent: 전달한 학생 객체의 필드 값을 읽어서 출력한다.
