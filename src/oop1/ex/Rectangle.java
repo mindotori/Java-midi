3b9f2773366fd189bc782f9327f30d59fdc2840a
mindotori <148763440+mindotori@users.noreply.github.com>
2024-08-27 18:33:43 +0900

    JAVA: 사각형 둘레 너비 구하기n]

--- src/oop1/ex/Rectangle.java
+++ src/oop1/ex/Rectangle.java
@@ -1,4 +1,16 @@
 package oop1.ex;
 
 public class Rectangle {
+    int width;
+    int height;
+
+     int calculateArea() { //매개변수 필요없다
+        return width * height;
+    }
+     int calculatePerimeter() {
+        return 2 * (width + height);
+    }
+     boolean isSquare() {
+        return width == height;
+    }
 }
