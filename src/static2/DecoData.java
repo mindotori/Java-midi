64ac793691ffc84230917ac0f7ebe10d7f303976
mindotori <148763440+mindotori@users.noreply.github.com>
2024-08-30 13:41:12 +0900

    JAVA: static 메서드

--- src/static2/DecoData.java
+++ src/static2/DecoData.java
@@ -6,11 +6,25 @@ public class DecoData {
     private static int staticValue;
 
     public static void staticCall() {
+        // static은 static만 접근 가능
         //instanceValue++; //인스턴스 변수 접근. 컴파일 에러
         //instanceMethod(); //인스턴스 메서드 접근, 컴파일 에러
 
         staticValue++; // 정적 변수에 접근
         staticMethod(); //정적 메서드 접근
+    }
+
+    public static void staticCall(DecoData data){
+        data.instanceValue++;
+        data.instanceMethod();
+    }
+
+    public void instanceCall(){
+        instanceValue++; /// 인스턴스 변수 접근
+        instanceMethod(); //인스턴스 메서드 접근
+
+        staticValue++;
+        staticMethod(); // static 영역도 접근 가능
     }
 
     private void instanceMethod(){
