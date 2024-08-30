38ac7fdb5b3583239f2331db866764bce4649ac4
mindotori <148763440+mindotori@users.noreply.github.com>
2024-08-30 23:06:05 +0900

    JAVA: import static

--- src/static2/DecoDataMain.java
+++ src/static2/DecoDataMain.java
@@ -1,10 +1,16 @@
 package static2;
 
+//import static static2.DecoData.*; // 임포트 스태틱 (클래스명 생략가능)
+import static static2.DecoData.*;
 public class DecoDataMain {
 
     public static void main(String[] args) {
         System.out.println("1. 정적 호출");
-        DecoData.staticCall();
+        DecoData.staticCall(); //임포트하지 않은 경우 (정석)
+        staticCall(); // 임포트 스태틱
+        staticCall();
+        staticCall();
+        staticCall();
 
         System.out.println("2. 인스턴스 호출1");
         DecoData data1 = new DecoData();
@@ -14,6 +20,13 @@ public class DecoDataMain {
         DecoData data2 = new DecoData();
         data2.instanceCall();
 
-        DecoData.staticCall(data1);
+        //추가
+        //인스턴스를 통한 접근
+        DecoData data3 = new DecoData();
+        data3.staticCall();//밑과 똑같은 결과지만 권장하지 않음
+
+        //클래스를 통한 접근
+        staticCall(); //스태틱 메서드
+
     }
 }
