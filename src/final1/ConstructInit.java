9e8657602f302d9305ffb4702a36691e711fd5a3
mindotori <148763440+mindotori@users.noreply.github.com>
2024-08-30 23:55:54 +0900

    JAVA: static 변수값 설정

---
+++ src/final1/ConstructInit.java
@@ -0,0 +1,10 @@
+package final1;
+
+public class ConstructInit {
+
+    final int value;
+
+    public ConstructInit(int value) {
+        this.value = value; //생성자를 통해 값을 할당해야하고 이후에는 값 수정 불가
+    }
+}
