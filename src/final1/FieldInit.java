8b16c93ecfee69c4f818bb730c261c75df8666f0
mindotori <148763440+mindotori@users.noreply.github.com>
2024-09-01 03:38:12 +0900

    JAVA: static final

--- src/final1/FieldInit.java
+++ src/final1/FieldInit.java
@@ -2,10 +2,10 @@ package final1;
 
 public class FieldInit {
 
-    static final int CONST_VALUE = 10;
+    static final int CONST_VALUE = 10; //상수: static final 키워드 사용 - 대문자 사용, 구분은 _로
     final int value = 10;
 
-    public FieldInit(int value){
-        // this.value = value; // 초기값 설정했기 때문에 생성자에서도 선언 불가
-    }
+    //public FieldInit(int value){
+        // this.value = value; // 컴파일 오류 - 초기값 설정했기 때문에 생성자에서도 선언 불가
+   // }
 }
