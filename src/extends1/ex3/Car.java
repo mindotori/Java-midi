4de2fb589829f5bc89b0e87dede6eaa0c88be8b5
mindotori <148763440+mindotori@users.noreply.github.com>
2024-09-03 15:10:10 +0900

    JAVA: 상속

---
+++ src/extends1/ex3/Car.java
@@ -0,0 +1,13 @@
+package extends1.ex3;
+
+public class Car {
+
+    public void move(){
+        System.out.println("차를 이동합니다.");
+    }
+
+    //추가 - 자식들은 모두 이 기능을 물려받게 된다.(상속)
+    public void openDoor() {
+        System.out.println("문을 엽니다.");
+    }
+}
