4ba16e01adb77fbb1b38dee51ef99be19063d1bc
mindotori <148763440+mindotori@users.noreply.github.com>
2024-09-09 01:35:42 +0900

    JAVA:다형성 예제 수정

--- src/poly/ex/pay1/PayMain2.java
+++ src/poly/ex/pay1/PayMain2.java
@@ -9,11 +9,11 @@ public class PayMain2 {
         PayService payService = new PayService();
 
         while(true){
-            System.out.println("결제 수단을 입력하세요:");
+            System.out.print("결제 수단을 입력하세요:");
             String payOption = sc.nextLine();
 
             if(payOption.equals("exit")){
-                System.out.println("프로그램을 종료합니다.");
+                System.out.print("프로그램을 종료합니다.");
                 return;
             }
 
