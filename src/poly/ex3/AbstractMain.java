85fc667da2a8d37f5f984eb9a4a785c907f3b871
mindotori <148763440+mindotori@users.noreply.github.com>
2024-11-04 23:07:32 +0900

    추상클래스 예제

--- src/poly/ex3/AbstractMain.java
+++ src/poly/ex3/AbstractMain.java
@@ -20,6 +20,7 @@ public class AbstractMain {
         soundAnimal(duck);
     }
 
+    //동물이 추가 되어도 변하지 않는 코드
     private static void soundAnimal(AbstractAnimal animal){
         System.out.println("동물 소리 테스트 시작");
         animal.sound();
--- src/poly/ex3/Dog.java
+++ src/poly/ex3/Dog.java
@@ -1,9 +1,9 @@
 package poly.ex3;
 
-public class Dog extends AbstractAnimal{
+public class Dog extends AbstractAnimal { //추상클래스 상속받음
 
     @Override
     public void sound() {
         System.out.println("멍멍");
-    }
+    } //오버라이딩
 }
